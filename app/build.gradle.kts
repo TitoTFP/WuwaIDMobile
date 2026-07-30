05cc16fb4531ef3715b1796dcd6f4ab91b8617e2
TitoTFP <titofp371@gmail.com>
2026-07-30 18:51:20 +0700

    release: prepare v0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.titotfp.wuwaid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndkVersion = "27.0.12077973"
